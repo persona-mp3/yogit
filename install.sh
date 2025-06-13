d71cfe72aa4b270a93ed13978bd47dae16b59a7f
ARCHIVE <archive>
2025-06-13 14:54:46 +0100

    bugs fixed in bash script

--- install.sh
+++ install.sh
@@ -2,16 +2,20 @@
 
 echo "Installing and configuring Yogit" 
 echo "Building executable..."
-#$(cd ~/test_clone_ci && go build main.go)
-# $(cd ~/.config/yogit && go build main.go)
 $(go build main.go)
 
 windows="main.exe"
 linux="main"
 
-if [ -f "$linux"]; then
+if [ -f "$linux" ]; then
   echo "Building for linux systems..."
-  BuildLinux()
+  BuildLinux
+else 
+  echo "Building for windows system"
+  curr_dir=pwd
+  exec_path=$curr_dir/$linux
+  echo "fatal: Build failed, please refer to docs at https://github.com/persona-mp3/yogit.git"
+  echo "You can directly install the build from the repo"
 fi
 
 
@@ -28,27 +32,4 @@ BuildLinux(){
   echo "--- Confirm config was successful"
   echo "Run `yogit` to confirm or check .bashrc for $config"
 }
-#cd ~/test_clone_ci
-cd ~/.config/yogit
-echo ""
-
-#echo "changing directory"
-
-#echo "cd ~/test_clone_ci"
-
-#exec_path=$HOME/test_clone_ci/main.exe
-echo "Building execution path"
-exec_path=$HOME/.config/yogit/main
-
-config="alias yogit=$exec_path"
-echo "---Writing to .bashrc---"
-echo $config
-
-echo $config >> ~/.bashrc
-echo "Sourcing .bashrc"
-source ~/.bashrc
-
-#echo "check .bashrc file to confirm alias is successfully configured as yogit2"
-echo "---Confirm setup process was successful---"
-cat -n ~/.bashrc
 
